cf729642c5713e583a787b5d71a80d10a22f5697
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-17 00:47:03 +0800

    replace and output

--- Decryptor.py
+++ Decryptor.py
@@ -1,7 +1,8 @@
 import pyfiglet
-import termcolor 
 
 # ask user for input
 input_text = input('\033[1m' + "Type the Encrypted text: " + '\033[0m')
 # replace corresponding symbols *, &, #, +, !
+new_text = input_text.replace('*', 'a').replace('&', 'e').replace('#', 'i').replace('+', 'o').replace('!', 'u')
 # print output
+print('\033[1m' + pyfiglet.figlet_format(new_text, font = "standard"))
