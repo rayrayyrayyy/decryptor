4a503cf478b5a0a25bc404330998115e02e14c3a
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-17 00:25:42 +0800

    input

--- Decryptor.py
+++ Decryptor.py
@@ -2,5 +2,6 @@ import pyfiglet
 import termcolor 
 
 # ask user for input
+input_text = input('\033[1m' + "Type the Encrypted text: " + '\033[0m')
 # replace corresponding symbols *, &, #, +, !
 # print output
