6d1e87177b3d0e450bd7a6b5df88efe44691eddf
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-17 01:05:27 +0800

    designnn

--- Decryptor.py
+++ Decryptor.py
@@ -1,8 +1,20 @@
 import pyfiglet
+from termcolor import colored, cprint
+
+print_yellow = lambda x: cprint(x, 'yellow')
+print_red = lambda x: cprint(x, 'red')
 
 # ask user for input
+print('\n' + '=====' * 25)
 input_text = input('\033[1m' + "Type the Encrypted text: " + '\033[0m')
 # replace corresponding symbols *, &, #, +, !
 new_text = input_text.replace('*', 'a').replace('&', 'e').replace('#', 'i').replace('+', 'o').replace('!', 'u')
+
+print_red("\nDECRYPTING, PLEASE WAIT" + "\033[5m...\n")
+import time
+time.sleep (3)
+
 # print output
-print('\033[1m' + pyfiglet.figlet_format(new_text, font = "standard"))
+print('`````' * 25)
+print_yellow('\033[1m' + pyfiglet.figlet_format(new_text, font = "standard"))
+print('`````' * 25)
